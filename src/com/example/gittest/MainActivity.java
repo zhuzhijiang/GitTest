2ef09ce2a2d7adf10748e45dc2074dec1c9bc09c
zzj <501155170@qq.com>
2015-12-07 15:37:41 +0800

    添加按钮

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.gittest;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	Button btn_click ;
+	TextView text;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		btn_click = (Button)findViewById(R.id.btn_ok);
+		text = (TextView)findViewById(R.id.tv_show);
+		
+		btn_click.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				text.setText("OK");
+			}
+		});
 	}
 
 	@Override
